39bdf851ef512518acbec58b9ca1758ec798f1f2
Fugas <77245219+Fugascz@users.noreply.github.com>
2021-07-22 00:50:24 +0200

    Revert "Optimalization"
    This reverts commit 7517e27cdffc2ccc66089bfbbbbf69a4cc9f2a24.

--- server/main-s.lua
+++ server/main-s.lua
@@ -6,21 +6,22 @@ AddEventHandler('fgs-duty:set', function(job)
     local _source = source
     local xPlayer = ESX.GetPlayerFromId(_source)
 
-    local offDuty = string.format('off%s', xPlayer.job.name)
-    local onDuty = string.gsub(xPlayer.job.name, 'off', '')
+    local playerJob = xPlayer.job.name
+    local playerGrade = xPlayer.job.grade
 
-    for k,v in pairs(Config.Duty) do
-        if v.Jobs[xPlayer.job.name] and onDuty then
-            xPlayer.setJob(offDuty, xPlayer.job.grade)
-            TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = 'Jsi mimo službu.'})
+    local offDuty = string.format('off%s', playerJob)
+    local onDuty = string.gsub(playerJob, 'off', '')
 
-            DiscordWebhook('**OFF DUTY**', string.format('Hráč: **%s (%s)** \n Odešel ze služby: **%s** - **%s**', GetPlayerName(_source), xPlayer.getIdentifier(), xPlayer.job.label, xPlayer.job.grade_label))
-        elseif v.Jobs[xPlayer.job.name] and offDuty then
-            xPlayer.setJob(onDuty, xPlayer.job.grade)
-            TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = 'Jsi ve službě.'})
-        
-            DiscordWebhook('**ON DUTY**', string.format('Hráč: **%s (%s)** \n Přišel do služby: **%s** - **%s**', GetPlayerName(_source), xPlayer.getIdentifier(), xPlayer.job.label, xPlayer.job.grade_label))
-        end
+    if playerJob == 'police' or playerJob == 'sheriff' or playerJob == 'ambulance' then
+        xPlayer.setJob(offDuty, playerGrade)
+        TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = 'Jsi mimo službu.'})
+
+        DiscordWebhook('**OFF DUTY**', string.format('Hráč: **%s (%s)** \n Odešel ze služby: **%s** - **%s**', GetPlayerName(_source), xPlayer.getIdentifier(), xPlayer.job.label, xPlayer.job.grade_label))
+    elseif playerJob == 'offpolice' or playerJob == 'offsheriff' or playerJob == 'offambulance' then
+        xPlayer.setJob(onDuty, playerGrade)
+        TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = 'Jsi ve službě.'})
+       
+        DiscordWebhook('**ON DUTY**', string.format('Hráč: **%s (%s)** \n Přišel do služby: **%s** - **%s**', GetPlayerName(_source), xPlayer.getIdentifier(), xPlayer.job.label, xPlayer.job.grade_label))
     end
 end)
 
